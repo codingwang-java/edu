57a0e6fd0b4d0a2756801c8ad9b7d30ee0740b3e
dejavu <wangzhengquan111@126.com>
2021-03-07 02:43:51 +0800

    commit for docker mysql

---
+++ pilot_user/src/main/java/com/dejavu/edu/mapper/EduAuthorityMapper.java
@@ -0,0 +1,16 @@
+package com.dejavu.edu.mapper;
+
+import com.dejavu.edu.entity.EduAuthority;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author dejavu
+ * @since 2021-03-07
+ */
+public interface EduAuthorityMapper extends BaseMapper<EduAuthority> {
+
+}
---
+++ pilot_user/src/main/java/com/dejavu/edu/mapper/EduUserMapper.java
@@ -0,0 +1,16 @@
+package com.dejavu.edu.mapper;
+
+import com.dejavu.edu.entity.EduUser;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author dejavu
+ * @since 2021-03-07
+ */
+public interface EduUserMapper extends BaseMapper<EduUser> {
+
+}
